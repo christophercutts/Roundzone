067ee8a30b99fcd4ad1583654b0ae2fbd5719d29
Christopher Cutts <chris@chriscutts.co.uk>
2015-02-20 23:55:49 +0000

    Added Character controller

---
+++ app/Character/Controller/Character.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clutz
+ * Date: 20/02/2015
+ * Time: 21:56
+ */
+
+class Character_Controller_Character extends Rz_Controller_Abstract {
+
+    function newAction() {
+        echo "Create a new character";
+    }
+
+    function viewAction($id) {
+
+        $this->loadLayout($id);
+    }
+
+    function testAction() {
+
+    }
+
+}
